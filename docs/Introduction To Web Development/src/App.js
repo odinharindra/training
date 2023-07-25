670037919cbb543fe8f6a6154a6298181f3f66aa
Harindra Masna <86703116+harindra-dev@users.noreply.github.com>
2023-07-25 23:03:03 +0530

    introduction to web development react app

---
+++ docs/Introduction To Web Development/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header></header>
+      <aside className='left'></aside>
+      <main></main>
+      <aside className='right'></aside>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
