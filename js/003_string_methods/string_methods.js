359247fd67efe684503ec7d4fb670bc843ffb93a
Harindra Masna <86703116+harindra-dev@users.noreply.github.com>
2023-08-19 21:17:05 +0530

    JS: String methods

---
+++ js/003_string_methods/string_methods.js
@@ -0,0 +1,20 @@
+const full_name = "Harindra Masna";
+
+console.log("full_name: ", full_name);
+
+// String.prototype.toLowerCase();
+console.log("full_name: ", full_name.toLowerCase());
+
+// String.prototype.toUpperCase();
+console.log("full_name: ", full_name.toUpperCase());
+
+// String.prototype.replace();
+console.log("full_name: ", full_name.replace("Harindra", "Harindra Narayana")); // return: "Harindra Narayana Masna"
+
+// String.prototype.split();
+console.log("full_name: ", full_name.split(" ")); // return: ['Harindra', 'Masna']
+
+// String.prototype.substring();
+console.log("full_name: ", full_name.substring(0, 3));
+
+// We can declare any variable with `const`, `let` and `var`
